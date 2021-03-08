2b1b1854f4cd4fa0c0970169ca6990a14bf3482c
Muhammad Anza Khan <muhammadanza.khan@ceridian.com>
2021-03-08 13:25:51 -0500

    comparison method

--- RayTracerChallenge/Program.cs
+++ RayTracerChallenge/Program.cs
@@ -8,6 +8,13 @@ namespace RayTracerChallenge
         {
             //testing my Matrix4 class
             Matrix4 m = new Matrix4(4, 4);
+
+            //testing comparison class
+            Matrix4 m1 = new Matrix4(4, 4);
+            Matrix4 m2 = new Matrix4(4, 4);
+
+            //m2.matrix[0, 0] = 5;
+            Console.WriteLine(Matrix4.Matrix4MatrixComparison(m1.matrix, m2.matrix));
         }
     }
 }
