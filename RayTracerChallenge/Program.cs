0fdeffc35407a5fae568c76cfa45693892ad1e7a
Muhammad Anza Khan <muhammadanza.khan@ceridian.com>
2021-03-08 13:16:12 -0500

    Initial commit for creation of 4x4 matrix. Can create it

--- RayTracerChallenge/Program.cs
+++ RayTracerChallenge/Program.cs
@@ -6,11 +6,8 @@ namespace RayTracerChallenge
     {
         static void Main(string[] args)
         {
-            Canvas can = new Canvas(100, 100);
-            can.CanvasWritePixel(50, 50, new Colour(1f, 0f, 0f));
-            can.CanvasWritePixel(50, 51, new Colour(0f, 1f, 0f));
-            can.CanvasWritePixel(50, 52, new Colour(0f, 0f, 1f));
-            PPMCreater.PPMCreaterImageFile(can, 255);
+            //testing my Matrix4 class
+            Matrix4 m = new Matrix4(4, 4);
         }
     }
 }
