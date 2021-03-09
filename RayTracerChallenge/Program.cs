31634d0ce4ac1853b86c6e0838d0e1425fe0720c
Muhammad Anza Khan <muhammadanza.khan@ceridian.com>
2021-03-09 17:24:01 -0500

    Completed chapter 4

--- RayTracerChallenge/Program.cs
+++ RayTracerChallenge/Program.cs
@@ -8,8 +8,8 @@ namespace RayTracerChallenge
         {
 
             Matrix4 m2 = new Matrix4(4, 4);
-            Matrix4 rotatZ = m2.Matrix4MatrixRotationZAxis(Math.PI / 4);
-            Point p3 = new Point(0, 1, 0);
+            Matrix4 rotatZ = m2.Matrix4MatrixShear(xZ:1.0f);
+            Point p3 = new Point(2, 3, 4);
 
             Point p4 = p3.PointMatrixMultiplication(p3, rotatZ);
         }
