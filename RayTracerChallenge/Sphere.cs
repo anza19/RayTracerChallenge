a04f6059d2cd4ba5640454987e814f5cd11c5329
Muhammad Anza Khan <muhammadanza.khan@ceridian.com>
2021-03-10 10:53:15 -0500

    Added logic to compute points of intersection between ray and sphere

---
+++ RayTracerChallenge/Sphere.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace RayTracerChallenge
+{
+    public class Sphere
+    {
+        public Point origin;
+        public int radius;
+        public static int sphereId = 0;
+
+
+        //our sphere is simplified by being centered at the origin and having unit length
+        public Sphere() 
+        {
+            origin = new Point(x: 0, y: 0, z: 0);
+            radius = 1;
+
+            Console.WriteLine($"sphereId: {sphereId}");
+
+            //ensuring each sphereId is unique
+            sphereId++;
+        }
+    }
+}
