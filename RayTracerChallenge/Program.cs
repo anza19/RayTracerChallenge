5606e62c37a3fce24c87ad06bffce8ec7d00494d
Muhammad Anza Khan <muhammadanza.khan@ceridian.com>
2021-03-09 20:01:11 -0500

    Ray class created

--- RayTracerChallenge/Program.cs
+++ RayTracerChallenge/Program.cs
@@ -6,12 +6,8 @@ namespace RayTracerChallenge
     {
         static void Main(string[] args)
         {
-
-            Matrix4 m2 = new Matrix4(4, 4);
-            Matrix4 rotatZ = m2.Matrix4MatrixShear(xZ:1.0f);
-            Point p3 = new Point(2, 3, 4);
-
-            Point p4 = p3.PointMatrixMultiplication(p3, rotatZ);
+            Ray r1 = new Ray(new Point(2, 3, 4), new Vector(1, 0, 0));
+            Point x = r1.RayPosition(r1, 1);
         }
     }
 }
