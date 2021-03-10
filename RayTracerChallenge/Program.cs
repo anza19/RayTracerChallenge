5f0d1bc52f5f1c55d7fc48871114912c94e6ebe9
Muhammad Anza Khan <muhammadanza.khan@ceridian.com>
2021-03-10 10:53:15 -0500

    Added logic to compute points of intersection between ray and sphere

--- RayTracerChallenge/Program.cs
+++ RayTracerChallenge/Program.cs
@@ -8,6 +8,13 @@ namespace RayTracerChallenge
         {
             Ray r1 = new Ray(new Point(2, 3, 4), new Vector(1, 0, 0));
             Point x = r1.RayPosition(r1, 1);
+
+            Sphere sp = new Sphere();
+            Ray r = new Ray(new Point(0, 0, 0), new Vector(0, 0, 1));
+
+            float[] intersection = r.RaySphereIntersection(r, sp);
+            Console.WriteLine(intersection[0]);
+            Console.WriteLine(intersection[1]);
         }
     }
 }
--- RayTracerChallenge/Ray.cs
+++ RayTracerChallenge/Ray.cs
@@ -31,6 +31,32 @@ namespace RayTracerChallenge
             return p;
         }
 
+        public float[] RaySphereIntersection(Ray r, Sphere s)
+        {
+            Vector sphereToRay = r.origin.PointPointSubtraction(r.origin, s.origin);
+            float a = r.direction.VectorDotProduct(r.direction, r.direction);
+            float b = 2 * r.direction.VectorDotProduct(r.direction, sphereToRay);
+            float c = r.direction.VectorDotProduct(sphereToRay, sphereToRay) - 1.0f;
+
+            float discrimnant = (b * b) - (4 * a * c);
+
+            if(discrimnant < 0)
+            {
+                //no intersection, return nothing
+                return null;
+            }
+
+            float discrimnantRoot = (float)(Math.Sqrt(discrimnant));
+
+            float t1 = (-b - discrimnantRoot) / (2 * a);
+            float t2 = (-b + discrimnantRoot) / (2 * a);
+
+            if(t1 >= t2)
+            {
+                return new float[] { t1, t2 };
+            }
+            return new float[] { t2, t1 };
+        }
 
     }
 }
