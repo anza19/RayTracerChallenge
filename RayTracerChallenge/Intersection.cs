9179734990fb695c75dbab6b71a4e1f1fb1cfa66
Muhammad Anza Khan <muhammadanza.khan@ceridian.com>
2021-03-13 18:12:47 -0500

    Added hit method which returns the visible intersection

--- RayTracerChallenge/Intersection.cs
+++ RayTracerChallenge/Intersection.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace RayTracerChallenge
 {
@@ -19,6 +19,38 @@ namespace RayTracerChallenge
         {
             this.t = t;
             this.sphereIntersected = sphereIntersected;
+        }
+
+        //this method returns the hit from a collection of intersection records
+        //the hit is the visiible intersection
+        //that is the object from a list of intersections that we can actually see and ultimately render
+        //hit will never be behind the ray's origin, since that's effectively behind the camera
+        //so you can ignore all intersections with negative t values
+        public static Intersection Hit(List<Intersection> intersections)
+        {
+            if (intersections.Count <= 0)
+            {
+                Console.WriteLine("no intersection took place");
+                return null;
+            }
+
+            //if both intersections are negative, return nothing
+            if(intersections.Last().t < 0 && intersections.First().t < 0)
+            {
+                return null;
+            }
+
+            //if just the smaller number is negative, return first element
+            else if(intersections.Last().t < 0 && intersections.First().t > 0)
+            {
+                return intersections.First();
+            }
+
+            //just return the last number
+            else
+            {
+                return intersections.Last();
+            }    
         }
     }
 }
--- RayTracerChallenge/Program.cs
+++ RayTracerChallenge/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RayTracerChallenge
 {
@@ -6,15 +7,12 @@ namespace RayTracerChallenge
     {
         static void Main(string[] args)
         {
-            Ray r1 = new Ray(new Point(2, 3, 4), new Vector(1, 0, 0));
-            Point x = r1.RayPosition(r1, 1);
-
             Sphere sp = new Sphere();
             Ray r = new Ray(new Point(0, 0, 0), new Vector(0, 0, 1));
 
-            /*float[] intersection = r.RaySphereIntersection(r, sp);
-            Console.WriteLine(intersection[0]);
-            Console.WriteLine(intersection[1]);*/
+            List<Intersection> intersections = r.RaySphereIntersection(r, sp);
+            Intersection hit = Intersection.Hit(intersections);
+            Console.WriteLine($"{hit.t}, {hit.sphereIntersected.radius}");
         }
     }
 }
