c73a6ad417cf4708839cc6a427121c262e2574e9
Muhammad Anza Khan <muhammadanza.khan@ceridian.com>
2021-03-17 10:01:17 -0400

    Added method to compute surface normal vector

--- RayTracerChallenge/Program.cs
+++ RayTracerChallenge/Program.cs
@@ -7,7 +7,15 @@ namespace RayTracerChallenge
     {
         public static void Main(string[] args)
         {
-            Chapter5Challenge.Chapter5RayCaster();
+            //Chapter5Challenge.Chapter5RayCaster();
+
+            Sphere s = new Sphere();
+            Matrix4 translationMatrix = new Matrix4(4, 4);
+            translationMatrix = translationMatrix.Matrix4MatrixTranslation(0, 1, 0);
+            s.SphereSetTransformation(translationMatrix);
+
+            Vector normal = s.SphereSurfaceNormalVector(s, new Point(0, 1.70711f, -0.70711f));
+        
         }
     }
 }
--- RayTracerChallenge/Sphere.cs
+++ RayTracerChallenge/Sphere.cs
@@ -32,5 +32,37 @@ namespace RayTracerChallenge
         {
             this.transformation = transformationMatrix;
         }
+
+        //getting surface normal vector
+        public Vector SphereSurfaceNormalVector(Sphere sphere, Point worldPoint)
+        {
+            //transform point from world space to object space
+
+            //invert transformation matrix
+            float[,] inverseTransformation = Matrix4.Matrix4MatrixInversion(sphere.transformation.matrix);
+            Matrix4 inverseTransformationMatrix = new Matrix4(4, 4);
+            inverseTransformationMatrix.matrix = inverseTransformation;
+
+            //multiply the inverted transformation matrix to the world point to transform it into
+            //object space
+            Point objectPoint = worldPoint.PointMatrixMultiplication(worldPoint, inverseTransformationMatrix);
+
+            //get the normal vector
+            //this is done by getting the vector that travels from the origin of the sphere to the objectPoint
+            Vector objectNormal = objectPoint.PointPointSubtraction(objectPoint, new Point(0, 0, 0));
+
+            //transform it into world space
+            //first get the transpose of the inverted transformation matrix
+            float[,] transposedInverted = Matrix4.Matrix4MatrixTranspose(inverseTransformationMatrix.matrix);
+            Matrix4 transposedInvertedMatrix = new Matrix4(4, 4);
+            transposedInvertedMatrix.matrix = transposedInverted;
+
+            //compute world normal
+            Vector worldNormal = objectNormal.VectorMatrixMultiplication(objectNormal, transposedInvertedMatrix);
+            worldNormal.w = 0f;
+
+            //normalize it
+            return worldNormal.VectorNormalization(worldNormal);
+        }
     }
 }
